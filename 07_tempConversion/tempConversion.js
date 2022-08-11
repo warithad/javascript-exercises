daa2dc585bd0f2fc9cc8d8aea59f24f28593e7ba
warith <warith.ad02@gmail.com>
2022-08-12 00:22:18 +0100

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(value) {
+ let ans = (value - 32) *(5/9);
+  return +ans.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(value) {
+  let ans = (value * (9/5)) + 32;
+  return +ans.toFixed(1);
 };
 
 // Do not edit below this line
