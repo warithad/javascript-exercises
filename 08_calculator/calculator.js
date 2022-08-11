c49f113fe9ef7b55654423f6bc96cd48b4d1038b
warith <warith.ad02@gmail.com>
2022-08-12 00:41:31 +0100

    Solve calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let ans = 0;
+  for(let val of array){
+    ans +=val;
+  }
+  return ans;
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let ans = 1;
+  for(let val of array){
+    ans*=val;
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return Math.pow(first, second);
 };
 
-const factorial = function() {
-	
+const factorial = function(val) {
+	let ans = 1;
+  for(let i = 1; i <= val; i++){
+    ans*=i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
