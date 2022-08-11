af3e5cc16117ab5f57037458ea52ae784d287df3
warith <warith.ad02@gmail.com>
2022-08-11 23:51:56 +0100

    Solve sumAll and leapYears problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if(first < 0 || second < 0|| typeof first !== typeof 1 || typeof second !== typeof 1){
+        return 'ERROR';
+    } 
 
+    let max = Math.max(first, second);
+    let min = Math.min(first, second);
+
+    let ans = Math.floor((max - min + 1)/2 * (max + min));
+    return ans;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(value) {
+    if(value < 0){
+        return false;
+    }
+
+    if(value <= 100){ 
+        if(value%4 == 0){
+            return true;
+       }else return false;
+    }
+
+    if(value%100 == 0){
+        return value % 400 == 0;
+    }
+
+    return value % 4 == 0;
 
 };
 
