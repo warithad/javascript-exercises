dfdd302a5f6526b1baba1302c1fb0ca7841a5830
warith <warith.ad02@gmail.com>
2022-08-12 01:06:03 +0100

    Solve palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (wor) {
+    let word = wor.toLowerCase();
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    word = word.replace(regex, '');
 
+    let i = 0;
+    let j = word.length- 1;
+
+    while(i < j){
+        if(word.charAt(i) == ' ')i++;
+        if(word.charAt(j) == ' ')j--;
+        if(word.charAt(i) != word.charAt(j)){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
