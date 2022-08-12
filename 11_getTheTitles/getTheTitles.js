c2558d2aba1525980bbf17aef80d920f457539f3
warith <warith.ad02@gmail.com>
2022-08-12 01:54:02 +0100

    Solve getTheTitles problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let ans = [];
+    for(let i = 0; i < books.length; i++){
+        ans.push(books[i].title);
+    }
+    return ans;
 };
 
 // Do not edit below this line
