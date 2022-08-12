cdc129ab9795bc33ad58789ed5528cbf6f6e52ea
warith <warith.ad02@gmail.com>
2022-08-12 01:29:30 +0100

    Solve fibonacci sequence problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(value) {
+    if(value < 0) return 'OOPS';
+    if(value == 1|| value == 2) return 1;
 
+    let a = 1;
+    let b = 1;
+
+    let ans = 0;
+
+    for(let i = 3; i <= value; i++){
+        ans = a+b;
+        a =b;
+        b = ans;
+    }
+
+    return ans;
 };
 
 // Do not edit below this line
